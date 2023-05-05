38114f925d5422569eb0b691c58bc26cf72f5053
Mahire Zühal <73248614+mahirezuhalozdemir@users.noreply.github.com>
2023-05-05 12:54:31 +0300

    Update login.js

--- Backend/login.js
+++ Backend/login.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const mysql = require("mysql2");
 const cors = require("cors");
 
+//veritabanı bağlantısı
 const db = mysql.createPool({
     host: "localhost",
     user: "root",
@@ -18,11 +19,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 module.exports.girisPOST = function (req, res) {
     //şifre ve eposta formdan gelecek
-    const k_adi = "user1";
+    const email = "user1";
     const sifre = "123";
 
-    var baglanti = db
-    baglanti.connect(function (hata) {
+    db.connect(function (hata) {
         if (!hata) {
             console.log("Giriş Post için Veri tabanına başarıyla bağlandı!");
         } else {
@@ -30,24 +30,21 @@ module.exports.girisPOST = function (req, res) {
         }
     }); // bağlan
 
-
-    sorgu = "select * from dietwhisperer.users where (email='" + k_adi + "') and (password='" + sifre + "')";
+    sorgu = "SELECT * FROM dietwhisperer.users WHERE (email='" + email + "') and (password='" + sifre + "')";
     baglanti.query(sorgu, function (hata, sonuc) {
-
         if (hata) {
-            console.log("\n\n\n Hata Oluştu! \n\n\n");
+            console.log("\n\n\n Hata Oluştu!");
             console.log(hata);
-
 
         } else {
 
-            if (sonuc==0) // sonuç yoksa yanlış k_adi ve şifre girmiştir.
+            if (sonuc==0) // sonuç yoksa yanlış bilgi girilmiştir
             {
-                req.flash('yanlisgiris', 'Yanlış kullanıcı adı veya şifre');
+                req.flash('Error', 'Yanlış email veya şifre');
                 res.locals.message = req.flash();
             } else {
 
-                req.session.admin = k_adi;
+                req.session.admin = email;
                 res.redirect("/home");
             }
         }
@@ -56,19 +53,6 @@ module.exports.girisPOST = function (req, res) {
 
 
 
-
-
-app.get("/register", (req, res) => {
-    const sqlInsert = "INSERT INTO dietwhisperer.users (email,name,surname,password,gender,age,weight,height) VALUES ('user1@gmail.com','user1','user1','123','female',34, 40.78, 170.0)";
-    db.query(sqlInsert, (err, result) => {
-        console.log("error", err);
-        console.log("result", result);
-        res.send("hello express");
-    });
-    res.send("hello express");
-});
-
-
-app.listen(5000, () => {
-    console.log("server is running on port 5000");
-});
+// app.listen(5000, () => {
+//     console.log("server is running on port 5000");
+// });
